540cd5ff430a4aeec2b3e1370d84e792844bff97
wangtao2001 <wangtao.cpu@gmail.com>
2021-10-20 22:50:06 +0800

    Finish registration

---
+++ static/js/register.js
@@ -0,0 +1,59 @@
+/*
+* 表单校验：用户名/密码不为空、两次密码一致、用户名不能超过15字符
+* */
+
+function username_check() {
+	let username =  document.getElementById("username").value;
+	let username_hint = document.getElementById("username_hint");
+	if (username === '') {
+		username_hint.innerHTML = "用户名不能为空";
+		return false;
+	} else if (username.length > 15) {
+		username_hint.innerHTML = "用户名不能超过15个字符";
+		return false;
+	} else {
+		username_hint.innerHTML = "";
+		return true;
+	}
+}
+
+function password1_check() {
+	let password1 = document.getElementById("password1").value;
+	let password1_hint = document.getElementById("password1_hint");
+	if (password1 === '') {
+		password1_hint.innerHTML = "密码不能为空";
+		return false;
+	} else {
+		password1_hint.innerHTML = "";
+		return true;
+	}
+}
+
+function password2_check() {
+	let password1 = document.getElementById("password1").value;
+	let password2 = document.getElementById("password2").value;
+	let password2_hint = document.getElementById("password2_hint");
+	if(password2 !== password1) {
+		password2_hint.innerHTML = "两次密码不一致";
+		return false;
+	} else {
+		password2_hint.innerHTML = "";
+		return true;
+	}
+}
+
+/*
+* 阻止表单提交 提示会存在短路
+*/
+function submit_check() {
+	return username_check() && password1_check() && password2_check();
+}
+
+/**
+ * 取消提示
+ */
+function tips_cancel() {
+	let tips = document.getElementById("tips");
+	tips.innerHTML = "";
+}
+
--- user/views.py
+++ user/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import *
+from . import models
+import hashlib
 
 # Create your views here.
 
@@ -8,6 +10,19 @@ def register_view(request: HttpRequest):
     if request.method == 'GET':
         return render(request, 'user/register.html')
     elif request.method == 'POST':
-        pass
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if models.User.objects.filter(username=username):
+            context = {
+                'tips': '用户名已存在'
+            }
+            return render(request, 'user/register.html', context=context)
+        m = hashlib.md5()
+        m.update(password1.encode())
+        password = m.hexdigest()
+        models.User.objects.create(username=username, password=password)
+        return HttpResponse('注册成功')  # 做一个重定向
     else:
         pass
